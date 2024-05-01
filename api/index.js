2fc5da8371bdef8d89678d3c85be338ae5094383
Irfan <irfantayyib07@gmail.com>
2024-05-01 22:50:09 +0500

    changed deployment settings + config

--- api/index.js
+++ api/index.js
@@ -3,12 +3,12 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger, logEvents } = require('./middleware/logger')
-const errorHandler = require('./middleware/errorHandler')
+const { logger, logEvents } = require('../middleware/logger')
+const errorHandler = require('../middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const connectDB = require('./config/dbConn')
+const corsOptions = require('../config/corsOptions')
+const connectDB = require('../config/dbConn')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
 
@@ -24,17 +24,17 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '', 'public')))
 
-app.use('/', require('./routes/root'))
-app.use('/auth', require('./routes/authRoutes'))
-app.use('/users', require('./routes/userRoutes'))
-app.use('/notes', require('./routes/noteRoutes'))
+app.use('/', require('../routes/root'))
+app.use('/auth', require('../routes/authRoutes'))
+app.use('/users', require('../routes/userRoutes'))
+app.use('/notes', require('../routes/noteRoutes'))
 
 app.all('*', (req, res) => {
  res.status(404)
  if (req.accepts('html')) {
-  res.sendFile(path.join(__dirname, 'views', '404.html'))
+  res.sendFile(path.join(__dirname, '..', 'views', '404.html'))
  } else if (req.accepts('json')) {
   res.json({ message: '404 Not Found' })
  } else {
--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,10 @@
 const allowedOrigins = [
  "https://www.henrysrepairshop.com",
  "https://www.henrysrepairshop.firebaseapp.com",
- "https://www.henrysrepairshop.web.app.",
- "https://www.henrysrepairshop.vercel.app.",
+ "https://www.henrysrepairshop.web.app",
+ "https://www.henrysrepairshop.vercel.app",
+ "https://henrysrepairs.vercel.app",
+ "http://localhost:3000",
 ]
 
 module.exports = allowedOrigins
